508802ea6dd7c393944aa30b1db197de9baa8dc3
_Pesto <pestonotpasta@gmail.com>
2023-09-03 23:03:40 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+import Navigation from "./components/header/Navigation";
+import Content from "./components/body/Content";
+
+function App() {
+    return (
+        <div>
+            <Navigation />
+            <Content />
+        </div>
+    );
+}
+
+export default App;
